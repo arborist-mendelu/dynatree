9f472ee0b36136fca429b86aee410b8227200f0d
robert-marik <robert.marik.cz@gmail.com>
2024-04-21 23:41:47 +0200

    image montage

---
+++ all_damping_one_page.sh
@@ -0,0 +1,13 @@
+# damping_2021-03-22_BK01_M02.png
+
+stromy=`ls damping/damping_*BK*png | grep "BK.*_" -o | sort | uniq`
+
+for i in $stromy
+do
+    echo $i
+    montage damping/damping*$i*png -tile 4x4 -geometry +0+0 damping/${i}_damping_all.png
+    montage damping/oscillations*$i*png -tile 4x4 -geometry +0+0 damping/${i}_oscillations_all.png
+done
+
+convert damping/*_damping_all.png damping/damping_all.pdf
+convert damping/*_oscillations_all.png damping/oscillations_all.pdf
