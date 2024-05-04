98d85e0749383dba904683f04493b6615ace4eff
robert-marik <robert.marik.cz@gmail.com>
2024-05-04 08:18:32 +0200

    ANOVA and t-test for frequency

--- plot_fft.py
+++ plot_fft.py
@@ -10,37 +10,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import statsmodels.api as sm
+import scipy
 
-probe = "Pt4"
+probe = "Pt3"
 df = pd.read_csv("results/fft.csv", dtype = {'tree': str, 'date': str})
 df = df[df["probe"].str.contains(probe)]
 df["leaves"] = False
 idx = (df["date"] == "2021-06-29") | (df["date"] == "2022-08-16")
 df.loc[idx,"leaves"] = True
+df = df.dropna()
 #%%
 
-df.reset_index(inplace=True)
-
-# %% boxplot
-
-# fig, ax = plt.subplots(figsize=(15,10))
-# sns.boxplot(
-#     data=df[(df["Freq"]>f_min) & (df["Delta freq"]<delta_f_min)], 
-#     x="tree", 
-#     y="Freq", 
-#     #style="tree", 
-#     hue="date", 
-#     palette=sns.color_palette("tab10")[:3],
-#     #s = 100,
-#     ax=ax
-#     )
-# # ax.set(ylim=(.16,.38))
-# [ax.axvline(x+.5,color='gray', lw=0.5) for x in ax.get_xticks()]
-# ax.legend(loc=2)
-# ax.grid(alpha=0.4)
-# ax.set(title="Základní frekvence")
-
-# %% swarmplot
 
 fig, axs = plt.subplots(2,1,figsize=(14,10), sharex=True)
 
@@ -83,3 +64,55 @@ ax.grid(alpha=0.4)
 # ax.set(title=f"Základní frekvence (dvě měření s listy a dvě bez listů) {probe}")
 # ax.set(ylim=(0,14))
 plt.savefig("outputs/boxplot.pdf")
+
+
+#%%
+
+df = df.reset_index(drop=True)
+
+#%%
+
+# ANOVA, t-test
+#
+# We start with transformation, since each tree has its own frequency.
+
+trees = df['tree'].pipe(np.unique)
+
+for tree in trees:
+    idx = df["tree"]==tree
+    mean = df.loc[idx,["freq"]].mean().values
+    std = df.loc[idx,["freq"]].std().values
+    df.loc[idx,["freq_diff_from_mean_rescaled"]] = (df.loc[idx,"freq"] - mean) / std
+
+#%%
+
+sm.qqplot(df[''], line='45', fit = True) 
+plt.show()
+
+#%%
+
+# https://gist.github.com/robert-marik/635affe37158d3fae1ef4f5bf3798dd8
+skupiny = [list(df.loc[i,'freq_diff_from_mean_rescaled']) for i in df.groupby(by='leaves').groups.values()]
+#print(skupiny) # kontrola prvku ve skupine
+#print(len(skupiny)) # kontrola poctu skupin
+for i in skupiny:
+    print(np.mean(i), np.std(i))
+
+print("ANOVA: ", scipy.stats.f_oneway(*skupiny))
+print("t-test: ", scipy.stats.ttest_ind(*skupiny, equal_var=True))
+
+
+#%%
+
+skupiny = [list(df.loc[i,'freq_diff_from_mean_rescaled']) for i in df.groupby(by='tree').groups.values()]
+#print(skupiny) # kontrola prvku ve skupine
+#print(len(skupiny)) # kontrola poctu skupin
+print("ANOVA: ", scipy.stats.f_oneway(*skupiny))
+
+#%%
+sns.boxplot(df, x="leaves", y="freq_diff_from_mean_rescaled")
+
+#%%
+sns.boxplot(df, x="tree", y="freq_diff_from_mean_rescaled")
+
+#%%
