e8aeb229381418b2db8479a75caae93e3395ebbd
robert-marik <robert.marik.cz@gmail.com>
2024-09-19 19:45:04 +0200

    FFT pomoci tukey

---
+++ lib_FFT.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 14:02:32 2024
+
+@author: marik
+"""
+
+import lib_dynatree as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal
+from scipy.fft import fft, fftfreq
+import lib_find_measurements
+from tqdm import tqdm
+import resource
+import matplotlib
+
+length = 60  # the length of the signal
+peak_min = .1 # do not look for the peak smaller than this value
+
+class DynatreeSignal:
+
+    def __init__(self, measurement, signal_source, release_source=None, dt=0.0002, tukey=0.1):
+        self.measurement = measurement
+        self.signal_source = signal_source
+        self.release_source = release_source
+        if self.release_source is None:
+            self.release_source = signal_source
+        if self.signal_source in self.measurement.data_pulling.columns:
+            self.signal_full = self.measurement.data_pulling[self.signal_source]
+            self.release_full = self.measurement.data_pulling[self.release_source]
+        elif self.signal_source in self.measurement.data_acc5000.columns:
+            self.signal_full = self.measurement.data_acc5000[self.signal_source]
+            self.release_full = self.measurement.data_acc5000[self.release_source]
+        elif self.signal_source in self.measurement.data_opticspt34.columns:
+            self.signal_full = self.measurement.data_opticspt34[self.signal_source]
+            self.release_full = self.measurement.data_opticspt34[self.release_source]
+        self.dt = dt
+        self.tukey = tukey
+
+    @property
+    def release_time(self):
+        data = self.release_full.dropna()
+        data = data - data.iloc[0]
+        data = data.dropna()
+        release = data.loc[20:].abs().idxmax()
+        return release
+    
+    @property
+    def signal(self):
+        signal = self.signal_full.loc[self.release_time:self.release_time+length]
+        signal = signal.dropna()
+        signal = signal - signal.mean()
+        
+        newindex = np.arange(signal.index[0], signal.index[0]+60+self.dt,self.dt)
+        newdata = np.interp(newindex, signal.index, signal.values, right=0)
+        
+        signal = pd.Series(index=newindex, data=newdata, name=signal.name)
+        tukey_window = scipy.signal.windows.tukey(len(signal), alpha=self.tukey, sym=False)
+        signal = signal * tukey_window
+        
+        return signal
+    
+    @property 
+    def fft(self):
+        N = self.signal.shape[0]  # get the number of points
+        xf_r = fftfreq(N, self.dt)[:N//2]
+        yf = fft(self.signal.values)  # preform FFT analysis
+        yf_r = 2.0/N * np.abs(yf[0:N//2])
+        df_fft = pd.Series(index=xf_r, data=yf_r, name=self.signal.name)
+        return df_fft
+    
+    @property
+    def main_peak(self):
+        return self.fft.loc[peak_min:].idxmax()
+
+df_failed_FFT_experiments=pd.read_csv("csv/FFT_failed.csv")
+    
+def plot_one_probe(day='2021-03-22', tree='BK01', measurement='M03', measurement_type='normal', probe='Elasto(90)'):
+    m = dt.DynatreeMeasurement(day=day, tree=tree, measurement=measurement, measurement_type=measurement_type)
+    s = DynatreeSignal(m, probe)
+    fig, ax = plt.subplots(2,1)
+    # print (s.release_time)
+    s.signal_full.plot(ax=ax[0])
+    s.signal.plot(ax=ax[0])
+    s.fft.plot(logy=True, xlim=(0,3), ax=ax[1])
+    ax[0].grid()
+    ax[1].grid()
+    ymax = s.fft.values.max()
+    ax[1].set(ylim=(ymax/10**4,ymax*2), xlabel="Freq / Hz", ylabel="Amplitude")
+    ax[0].set(xlim=(0,None), xlabel="Time / s", ylabel="Value")
+    plt.suptitle(f"{s.measurement} {s.signal_source}, {s.main_peak:.03f} Hz".replace("Dynatree measurement ",""))
+    test = [f"{measurement_type}", f"{day}", f"{tree}", f"{measurement}", f"{probe}"] in df_failed_FFT_experiments.values.tolist()
+    if test:
+        prefix = "FAILED_"
+        value = np.nan
+    else:
+        prefix = ""
+        value = s.main_peak
+        ax[1].axvline(s.main_peak, color='r', linestyle="--")
+    plt.tight_layout()
+    fig.savefig(f"../temp/fft_elasto/{prefix}{s.measurement.measurement_type}_{s.measurement.day}_{s.measurement.tree}_{s.measurement.measurement}_{s.signal_source}.png")
+    plt.close('all')
+    return value
+    # print(s.measurement.measurement_type, s.measurement.day, 
+    #       s.measurement.tree, s.measurement.measurement, s.signal_source,  s.main_peak)
+    
+    
+# plot_one_probe(tree="BK04")    
+
+# plot_one_probe(tree="BK09", measurement='M03', day="2023-07-17")    
+    
+#%%
+if __name__ == '__main__':
+    # resource.setrlimit(resource.RLIMIT_DATA, (100 * 1024 * 1024, 100 * 1024 * 1024)) 
+    matplotlib.use('TkAgg')
+
+    out = {}
+    df = lib_find_measurements.get_all_measurements(method='all', type='all')  
+    df = df[df["measurement"]!="M01"]
+    pbar = tqdm(total=len(df))
+    for i, row in df.iterrows():
+        date, tree, measurement, measurement_type, optics, day = row
+        pbar.set_description(f"{measurement_type} {day} {tree} {measurement}")
+        out[(measurement_type, day, tree, measurement)
+            ] = [plot_one_probe(day, tree, measurement, measurement_type, probe="Elasto(90)")]
+        pbar.update(1)
+
+    out_df = pd.DataFrame(out).T
+    out_df = out_df.reset_index(drop=False)
+    out_df.columns=["type","day","tree","measurement","peak"]
+    out_df.to_csv(f"../temp/Elasto.csv", index=False)
---
+++ plot_fft_boxplots_tukey.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  4 19:01:02 2023
+
+@author: marik
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def get_data(probe):
+    df = pd.read_csv("csv/solara_FFT.csv")
+    if probe in ["Inclino(80)", "Inclino(81)"]:
+    
+        df_axes = {}
+        for axis in ['X','Y']:
+            df_axes[axis] = ( df[df["probe"]==f"{probe}{axis}"]
+               .dropna(subset=['peaks', 'remark'], how='all')
+               .drop(["remark","from","to"], axis=1)
+               )
+        df = pd.concat(df_axes.values())    
+        df = df[["day","tree","measurement","peaks","probe"]]
+    else:
+    
+        df = df[df["probe"]==probe]
+        df = df[["day","tree","measurement","peaks","probe"]]
+    
+    
+    df["peak"] = df["peaks"].str.split(" ", expand=True).iloc[:,1]
+    df["leaves"] = "False"
+    idx = df["day"].isin(["2021-06-29", "2022-08-16", "2023-07-17"])
+    df.loc[idx,"leaves"] = "True"
+    idx = (df["day"].isin(["2024-04-10"])) & (df["tree"].isin(["BK01","BK08", "BK09", "BK11", "BK12", "BK21"]))
+    df.loc[idx,"leaves"] = "False & post reduction"
+    df["freq"] = df[["peak"]].astype(float)
+    df = df.dropna().sort_values(by="tree")
+    return df
+
+def plot_data(df,probe):
+    
+    fig, axs = plt.subplots(2,1,figsize=(14,10), sharex=True, sharey=True)
+    ax = axs[0]
+    # f_min = 0.1
+    # delta_f_min = 0.05
+    sns.swarmplot(
+        data=df, 
+        x="tree", 
+        y="freq", 
+        hue="leaves",
+        ax = ax
+        )
+    [ax.axvline(x+.5,color='gray', lw=0.5) for x in ax.get_xticks()]
+    ax.legend(title="Leaves")
+    ax.grid(alpha=0.4)
+    ax.set(title=f"Základní frekvence {probe}")
+    # ax.set(ylim=(0,14))
+    
+    ax = axs[1]
+    sns.boxplot(
+        data=df, 
+        x="tree", 
+        y="freq", 
+        hue="leaves",
+        ax = ax
+        )
+    [ax.axvline(x+.5,color='gray', lw=0.5) for x in ax.get_xticks()]
+    ax.legend(loc=2, title="Leaves")
+    ax.grid(alpha=0.4)
+    
+    axs[0].set(ylim=(0.1,0.5))
+    axs[1].set(ylim=(0.1,0.5))
+    return fig
+
+fig = {}
+for probe in ["Pt3", "Elasto(90)", "Inclino(80)", "Inclino(81)"]:
+    df = get_data(probe)
+    fig[probe] = plot_data(df, probe)
+    
+filename = "../outputs/fft_boxplots_for_probes.pdf"
+with PdfPages(filename) as pdf:
+    for f in fig.values():
+        pdf.savefig(f, bbox_inches='tight') 
