129ced6d9285879f4d82493d19da5ceb88be8ecb
robert-marik <robert.marik.cz@gmail.com>
2024-09-23 22:54:34 +0200

    uprava skriptu

--- static_pull_first_versus_other_pulls.py
+++ static_pull_first_versus_other_pulls.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# +
 """
 Created on Sat Sep 14 10:20:56 2024
 
@@ -12,7 +13,6 @@ import seaborn as sns
 import lib_dynatree
 from scipy.stats import ttest_1samp
 from scipy import stats
-
 
 def add_leaves_info(df_):
     df = df_.copy()
@@ -47,16 +47,20 @@ df = df.drop(["Intercept","p-value","stderr","intercept_stderr","lower_cut", "up
 a = df[(df["optics"]) & (df['Dependent'].str.contains('Pt'))]
 b = df[~df["optics"]]
 oridf = pd.concat([a,b]).reset_index(drop=True)
-oridf
+
+# -
 
 # # Je u prvního měření větší tuhost?
+#
+# Sledujeme statické měření. Určíme směrnice mezi veličinami, co nás zajímají. Je směrice v prvním tahu odlišná od směrnice ve druhém a dalším tazích? Vydělíme
+# druhou a třetí směrnici první směrnicí porovnáváme podíly s jedničkou. 
 
 df = oridf.copy()
 df = df[
     (df["measurement"]=="M01") & 
      (df["Independent"]=="M")
     ].drop(["measurement","optics"], axis=1)
-df
+
 
 # +
 # Nejprve vytáhneme hodnoty Slope pro pull=0
@@ -72,9 +76,8 @@ df_merged = pd.merge(df, df_zero_pull[['day', 'tree', 'Independent', 'Dependent'
 # Vydělení hodnoty Slope referenční hodnotou Slope_zero_pull
 for i in ['Slope']:
     df_merged[f'{i}_normalized'] = df_merged[i] / df_merged[f'{i}_zero_pull']
-# -
 
-df_merged
+# +
 
 target = "Slope"
 subdf = df_merged[df_merged["pullNo"]!=0].loc[:,["pullNo","Slope_normalized","tree"]]
@@ -83,6 +86,9 @@ subdf['pullNo'] = subdf['pullNo'].astype('category')
 sns.stripplot(data=subdf, x='tree', y=f'{target}_normalized', hue='pullNo', ax=ax, jitter=0.4)
 ax.grid()
 ax.set(ylim=(None,1.5))
+# -
+
+# Jestli jsou podíly rovny jedné nebo ne je možné otestovat i statistickým testem.
 
 # +
 result1 = subdf.groupby('tree')['Slope_normalized'].agg(['mean', 'std']).reset_index()
@@ -99,3 +105,5 @@ result = pd.merge(result1,result2, on=["tree"])
 result["H0_rejected"] = result["p_value"]<0.05
 result.sort_values(by="p_value").reset_index(drop=True)
 # -
+
+
