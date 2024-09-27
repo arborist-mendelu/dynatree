369a85bc646862fb78223852762e5c7f058577f8
robert-marik <robert.marik.cz@gmail.com>
2024-09-27 07:39:46 +0200

    filenames to configuration files

--- config.py
+++ config.py
@@ -7,13 +7,15 @@ Created on Fri Sep 27 07:03:03 2024
 """
 
 file = {}
+
 file['static_fail'] = "csv/static_fail.csv"
 file['static_checked_OK'] = "csv/static_checked_OK.csv"
-
 file['scale_factors'] = "csv/scale_factors.csv"
 file['reset_inclinometers'] = "csv/reset_inclinometers.csv"
 file['synchronization_finetune_inclinometers_fix'] = "csv/synchronization_finetune_inclinometers_fix.csv"
-file["outputs/regressions_static"] = "../outputs/regressions_static.csv"
 file['angles_measured'] = "csv/angles_measured.csv"
+
+file["outputs/regressions_static"] = "../outputs/regressions_static.csv"
 file['outputs/anotated_regressions_static'] = "../outputs/anotated_regressions_static.csv"
+
 file["tsv_dirs"] = "csv/tsv_dirs.csv"
