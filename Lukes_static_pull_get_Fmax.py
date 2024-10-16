75b98103811d43f0d2b3f0d367b58b91cece5bf1
Robert Marik <marik@mendelu.cz>
2024-10-16 14:14:03 +0200

    Lukes, extrakce dat

--- Lukes_static_pull_get_Fmax.py
+++ Lukes_static_pull_get_Fmax.py
@@ -1,9 +1,6 @@
-#%%
+# %%
 
 from lib_find_measurements import get_all_measurements
-import static_pull
-import rich
-import matplotlib.pyplot as plt
 import pandas as pd
 from static_pull import DynatreeStaticMeasurement
 
@@ -16,27 +13,21 @@ df = df[
 
 df
 
-
-
-#%%
+# %%
 
 data = {}
-for _,row in df.iloc[:,:].iterrows():
-    m = DynatreeStaticMeasurement(day=row['date'], tree=row['tree'], measurement=row['measurement'], measurement_type=row['type'], restricted=None)
-    for i,s in enumerate(m.pullings):
-        data[m.measurement_type,m.day,m.tree,m.measurement,i] = s.data['Force(100)'].max()
+for _, row in df.iloc[:, :].iterrows():
+    m = DynatreeStaticMeasurement(day=row['date'], tree=row['tree'], measurement=row['measurement'],
+                                  measurement_type=row['type'], restricted=None)
+    for i, s in enumerate(m.pullings):
+        data[m.measurement_type, m.day, m.tree, m.measurement, i] = s.data['Force(100)'].max()
 
-#%%
+# %%
 
 ans = pd.DataFrame(data, index=["Fmax"]).T
 ans
-#%%
+# %%
 
 grouped = ans.groupby(level=[0, 1, 2, 3]).max()
 grouped.to_csv("F_max.csv")
 grouped
-
-
-
-
-
--- acc2parquet.py
+++ acc2parquet.py
@@ -5,58 +5,60 @@ Created on Sep 03 2024
 
 Skript hledá acc soubory a prepisuje je do parquet. Meni frekvenci z 5000 na 100.
 
+UPOZORNENI: Tento soubor pravdepodobne nechces pouzit. Meni frekvenci samplovani.
+Pro jistotu je vse zapoznamkovano.
 
 @author: marik
 """
-
-import glob 
-import os
-import pandas as pd
-import scipy.io
-import logging
-logger = logging.getLogger("prevod")
-logger.setLevel(logging.DEBUG)
-from pathlib import Path
-
-from scipy.signal import savgol_filter
-
-source_dir = "/mnt/ERC/ERC"
-
-adresar = source_dir+'/Mereni_Babice'
-
-logger.debug("Starting to search directory")
-
-matching_dirs = glob.glob(os.path.join(adresar, '**/ACC'), recursive=True)
-
-def data(text):
-    t = text.split("/")
-    tree = t[-2]
-    date = t[-5].replace("_","-")
-    kind = t[-3]
-    return [date, kind, tree, text]
-
-upraveny_seznam = [data(i) for i in matching_dirs]
-df = pd.DataFrame(upraveny_seznam, columns=["date","kind","tree","directory"])
-
-#%%
-
-for i,row in df.iterrows():
-    files = glob.glob(os.path.join(row['directory'], '*.mat'))
-    for soubor in files:
-        print(soubor)
-        measurement = soubor.split("/")[-1].split(".")[-2]
-        target = f"../data/parquet_acc/{row['kind'].lower()}_{row['date']}_{row['tree']}_{measurement}.parquet"
-        file_path = Path(target)
-        if file_path.is_file():
-            # print(f"Soubor {target} existuje, koncim.")
-            continue
-        logger.info(f"Loading {soubor}")
-        # continue
-        mat = scipy.io.loadmat(soubor)
-        data = {k:mat[k].T[0] for k in mat.keys() if "Data1_A" in k}
-        smoothed_data = {k: savgol_filter(data[k], window_length=1000, polyorder=2)[::50] for k in data.keys()}  # Změna hodnot window_length a polyorder může ovlivnit úroveň vyhlazení
-        newdf = pd.DataFrame(dict([(key, pd.Series(value)) for key, value in smoothed_data.items()]))
-        newdf.to_parquet(target)
-        # logger.info(f"File {soubor} processed")        
-
-logging.info("Finished")
+#
+# import glob
+# import os
+# import pandas as pd
+# import scipy.io
+# import logging
+# logger = logging.getLogger("prevod")
+# logger.setLevel(logging.DEBUG)
+# from pathlib import Path
+#
+# from scipy.signal import savgol_filter
+#
+# source_dir = "/mnt/ERC/ERC"
+#
+# adresar = source_dir+'/Mereni_Babice'
+#
+# logger.debug("Starting to search directory")
+#
+# matching_dirs = glob.glob(os.path.join(adresar, '**/ACC'), recursive=True)
+#
+# def data(text):
+#     t = text.split("/")
+#     tree = t[-2]
+#     date = t[-5].replace("_","-")
+#     kind = t[-3]
+#     return [date, kind, tree, text]
+#
+# upraveny_seznam = [data(i) for i in matching_dirs]
+# df = pd.DataFrame(upraveny_seznam, columns=["date","kind","tree","directory"])
+#
+# #%%
+#
+# for i,row in df.iterrows():
+#     files = glob.glob(os.path.join(row['directory'], '*.mat'))
+#     for soubor in files:
+#         print(soubor)
+#         measurement = soubor.split("/")[-1].split(".")[-2]
+#         target = f"../data/parquet_acc/{row['kind'].lower()}_{row['date']}_{row['tree']}_{measurement}.parquet"
+#         file_path = Path(target)
+#         if file_path.is_file():
+#             # print(f"Soubor {target} existuje, koncim.")
+#             continue
+#         logger.info(f"Loading {soubor}")
+#         # continue
+#         mat = scipy.io.loadmat(soubor)
+#         data = {k:mat[k].T[0] for k in mat.keys() if "Data1_A" in k}
+#         smoothed_data = {k: savgol_filter(data[k], window_length=1000, polyorder=2)[::50] for k in data.keys()}  # Změna hodnot window_length a polyorder může ovlivnit úroveň vyhlazení
+#         newdf = pd.DataFrame(dict([(key, pd.Series(value)) for key, value in smoothed_data.items()]))
+#         newdf.to_parquet(target)
+#         # logger.info(f"File {soubor} processed")
+#
+# logging.info("Finished")
