53240c6bacd458afae75fc4410cf349f2e80ecf7
Robert Marik <marik@mendelu.cz>
2024-10-22 23:11:51 +0200

    service

--- run_service.sh
+++ run_service.sh
@@ -1,4 +1,5 @@
 eval "$(/home/marik/miniforge3/condabin/conda shell.bash hook)"
 /home/marik/miniforge3/condabin/conda activate dynatree
-/home/marik/miniforge3/envs/dynatree/bin/solara run solara_app.py --no-open --host=um-bc201.mendelu.cz
+#/home/marik/miniforge3/envs/dynatree/bin/solara run solara_app.py --no-open --host=um-bc201.mendelu.cz
+SOLARA_APP=solara_app.py /home/marik/miniforge3/envs/dynatree/bin/gunicorn --workers 4 --threads=20 -b 0.0.0.0:5000 app:app
 
