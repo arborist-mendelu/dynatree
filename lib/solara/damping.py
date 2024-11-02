c48f8674afeb57f272b154f98a82b88e557158c6
robert-marik <robert.marik.cz@gmail.com>
2024-11-02 11:12:13 +0100

    daping synchronizace grafu

--- lib/solara/damping.py
+++ lib/solara/damping.py
@@ -6,14 +6,14 @@ from lib_damping import DynatreeDampedSignal
 import plotly.graph_objects as go
 import numpy as np
 import plotly.express as px
-
+from plotly.subplots import make_subplots
+import pandas as pd
 import lib_dynatree
 import logging
 lib_dynatree.logger.setLevel(logging.INFO)
 
-def draw_signal_with_envelope(s, envelope=None, k=0, q=0, ):
+def draw_signal_with_envelope(s, fig, envelope=None, k=0, q=0, row=1, col=1 ):
     signal, time = s.damped_signal.reshape(-1), s.damped_time
-    fig = go.Figure()
     x = time
     y = np.exp(k * time + q)
     fig.add_trace(go.Scatter(x=np.concatenate([x, x[::-1]]),
@@ -21,14 +21,14 @@ def draw_signal_with_envelope(s, envelope=None, k=0, q=0, ):
                              fill='toself',
                              fillcolor='lightblue',
                              line=dict(color='lightblue'),
-                             showlegend=False))
-    fig.add_trace(go.Scatter(x=time, y=signal, mode='lines', name='signal', line=dict(color='blue')))
+                             showlegend=False), row=row, col=col)
+    fig.add_trace(go.Scatter(x=time, y=signal, mode='lines', name='signal', line=dict(color='blue')), row=row, col=col)
     if envelope is not None:
         fig.add_trace(
-            go.Scatter(x=time, y=envelope, mode='lines', name='envelope', line=dict(color='red'), legendgroup='obalka'))
+            go.Scatter(x=time, y=envelope, mode='lines', name='envelope', line=dict(color='red'), legendgroup='obalka'), row=row, col=col)
         fig.add_trace(go.Scatter(x=time, y=-envelope, mode='lines', showlegend=False, line=dict(color='red'),
-                                 legendgroup='obalka'))
-    fig.update_layout(xaxis_title="Čas", yaxis_title="Signál")
+                                 legendgroup='obalka'), row=row, col=col)
+    # fig.update_layout(xaxis_title="Čas", yaxis_title="Signál")
     return fig
 
 def resetuj(x=None):
@@ -54,11 +54,11 @@ def Page():
             # s.ImageSizes()
             with solara.Card(title="Signal source choice"):
                 solara.ToggleButtonsSingle(value=data_source, values=data_sources)
-    draw_images()
-    # try:
-    #     draw_images()
-    # except:
-    #     solara.Warning("Zatím jenom optika a Pt3. Ostatní měření a ostatní proby budou později.")
+    # draw_images()
+    try:
+        draw_images()
+    except:
+        solara.Warning("Někde nastala chyba.")
 
 def draw_images():
 
@@ -72,35 +72,32 @@ def draw_images():
         dt = 0.0002
     sig = DynatreeDampedSignal(m, data_source.value, dt=dt)
 
-    with solara.ColumnsResponsive(default=12, large=6, xlarge=4, wrap=True):
-        with solara.Card():
-            pass
-            envelope, k, q = sig.hilbert_envelope.values()
-            fig = draw_signal_with_envelope(sig, envelope, k, q)
-            fig.update_layout(title=f"Hilbertova obálka, k={k:.4f}<br>{m}",
-                              # height=s.height.value,
-                              # width=s.width.value,
-                              )
-            solara.FigurePlotly(fig)
+    data = {}
+    fig = make_subplots(rows=3, cols=1, shared_xaxes='all', shared_yaxes='all')
+    envelope, k, q = sig.hilbert_envelope.values()
+    fig = draw_signal_with_envelope(sig, fig, envelope, k, q, row=1)
+    data['hilbert'] = [k]
 
-        with solara.Card():
-            pass
-            peaks, k, q = sig.fit_maxima.values()
-            fig = draw_signal_with_envelope(sig, k=k, q=q)
-            fig.add_trace(go.Scatter(x=peaks.index, y=peaks.values.reshape(-1),
-                                     mode='markers', name='peaks', line=dict(color='red')))
-            fig.update_layout(title=f"Proložení exponenciely podle peaků, k={k:.4f}<br>{m}",
-                # height = s.height.value,
-                # width = s.width.value,
-                )
-            solara.FigurePlotly(fig)
+    peaks, k, q = sig.fit_maxima.values()
+    fig = draw_signal_with_envelope(sig, fig, k=k, q=q, row=2)
+    fig.add_trace(go.Scatter(x=peaks.index, y=peaks.values.reshape(-1),
+                             mode='markers', name='peaks', line=dict(color='red')), row=2, col=1)
+    data['extrema'] = [k]
 
-        with solara.Card():
-            # if "Pt" in data_source.value:
-            envelope, k, q, freq, fft_data = sig.wavelet_envelope.values()
-            fig = draw_signal_with_envelope(sig,k=k, q=q)
-            fig.update_layout(title=f"Proložení exponenciely pomocí waveletu, k={k:.4f}<br>{m}",
-                # height = s.height.value,
-                # width = s.width.value,
-                )
-            solara.FigurePlotly(fig)
+    envelope, k, q, freq, fft_data = sig.wavelet_envelope.values()
+    fig = draw_signal_with_envelope(sig, fig, envelope, k=k, q=q, row=3)
+    data['wavelets'] = [k]
+
+    fig.update_layout(title=f"Proložení exponenciely pomocí několika metod",
+                      height = 800,
+                      )
+
+    fig.update_yaxes(title_text="Hilbert", row=1, col=1)
+    fig.update_yaxes(title_text="Maxima/minima", row=2, col=1)
+    fig.update_yaxes(title_text="Wavelet", row=3, col=1)
+
+    df = pd.DataFrame.from_dict(data)
+    df.index = ['k']
+    solara.display(df)
+    solara.FigurePlotly(fig)
+
