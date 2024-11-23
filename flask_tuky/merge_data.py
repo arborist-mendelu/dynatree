b617864fa12182aad5a7cd90c0f07085d5a219d8
robert-marik <robert.marik.cz@gmail.com>
2024-11-23 23:43:31 +0100

    spojovani informaci o tucich

---
+++ flask_tuky/merge_data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+new = pd.read_csv("./flask_tuky/signal_ratio_upraveny.csv", index_col=0)
+new = new.set_index("file")[["liked"]]
+new.columns = ["valid"]
+new
+
+#%%
+
+data = pd.read_csv("./flask_tuky/FFT_acc_knock.csv", index_col=None)
+data["valid"] = True
+data['timecoords'] = data['day'].astype(str) + "_" + data['type']
+data = data.set_index("filename")
+
+#%%
+data.update(new["valid"])
+
+#%%
+data = data.reset_index()
+
+#%%
+data["manual_peaks"] = None
+#%%
+data.to_csv("upraveny_pro_import_3.csv")
+ #%%
+ data.columns
