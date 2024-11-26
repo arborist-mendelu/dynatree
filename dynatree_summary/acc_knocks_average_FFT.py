a9788ec2257b9869e5d0b88a89586ee3e3c92a9c
Robert Marik <marik@mendelu.cz>
2024-11-26 10:11:52 +0100

    prumerna FFT

--- dynatree_summary/acc_knocks_average_FFT.py
+++ dynatree_summary/acc_knocks_average_FFT.py
@@ -8,91 +8,94 @@ import matplotlib.pyplot as plt
 import matplotlib
 from parallelbar import progress_map
 
-logger.setLevel(logging.INFO)
+from dynatree.signal_knock import SignalTuk
+from dynatree.dynatree import DynatreeMeasurement, logger
+from dynatree.find_measurements import get_all_measurements_acc
+import logging
+
+logger.setLevel(logging.WARNING)
+
 CACHE = "../outputs/cache_FFTavg/"
 
-def get_FFT_all_acc(**kwds):
-    logger.info(f"get_FFT_all_acc entered, {kwds}")
-    probes = [f"a0{i}_{j}" for i in [1,2,3,] for j in ["x","y","z"]]
-    for probe in probes:
-        try:
-            get_FFT_one_probe(probe=probe, **kwds)
-        except:
-            logger.error(f"Failed get_FFT_one_probe for {kwds} and {probe}.")
+def get_one_fft(m=None, **kwds):
+    if m is None:
+        logger.info(f"Loading {m}")
+        m = DynatreeMeasurement(day=kwds['day'], tree=kwds['tree'], measurement=kwds['measurement'], measurement_type=kwds['measurement_type'])
+    s = SignalTuk(m, start=round(kwds['knock_time']/100.0 - 0.04, 2), end=round(kwds['knock_time']/100.0 + 0.04, 2), probe=kwds['probe'])
+    return s.fft
+
+def get_one_tree(**kwds):
+    measurement_type, day, tree = kwds['measurement_type'], kwds['day'], kwds['tree']
+    ms = df[(df[["measurement_type","day","tree"]]==[measurement_type, day, tree]).all(axis=1)]['measurement'].drop_duplicates().to_list()
+    m = {i: DynatreeMeasurement(day=day, tree=tree, measurement=i, measurement_type=measurement_type) for i in ms}
+    fft_table = df[ (df[["measurement_type","day","tree"]] == [measurement_type, day, tree]).all(axis=1)].copy()
+    fft_table = fft_table.set_index(["measurement", "knock_time", "probe"], drop=False)
+    ans = fft_table.apply(lambda row:get_one_fft(m=m[row['measurement']],**row), axis=1)
+    return ans.T
 
 
-def get_FFT_one_probe(**kwds):
-    logger.info(f"get_FFT_one_probe entered, {kwds}")
-    figname = f"{CACHE}FFTaverage_{kwds['type']}_{kwds['day']}_{kwds['tree']}_{kwds['probe']}.png"
-    csvname = f"{CACHE}FFTaverage_{kwds['type']}_{kwds['day']}_{kwds['tree']}_{kwds['probe']}.csv"
+def process_one_tree_to_images(**kwds):
+    logger.info(f"{kwds}")
+
     figsize = (10, 5)
-    columns_for_select = ["type", "day", "tree", "probe"]
-    subdf = df[(df[columns_for_select] ==
-                [kwds[i] for i in columns_for_select]).all(axis=1)].copy()
-    tuky = {}
-    subdf_iter = pd.DataFrame(subdf.groupby('measurement')['knock_time'].agg(list))
-    if len(subdf_iter)==0:
-        logger.warning(f"No data for get_FFT_one_probe, {kwds}, return empty image and empty dataframe")
+
+    ans = get_one_tree(**kwds)
+    probes = [f"a0{i}_{j}" for i in [1, 2, 3, 4] for j in ["x", "y", "z"]]
+
+    for probe in probes:
+        figname = f"{CACHE}FFTaverage_{kwds['measurement_type']}_{kwds['day']}_{kwds['tree']}_{probe}.png"
+        csvname = f"{CACHE}FFTaverage_{kwds['measurement_type']}_{kwds['day']}_{kwds['tree']}_{probe}.csv"
         fig, ax = plt.subplots(figsize=figsize)
-        ax.set(title=f"{kwds['type']} {kwds['day']} {kwds['tree']} {kwds['probe']}, data missing")
-        fig.savefig(figname, transparent=True)
-        ans = pd.DataFrame()
-        ans.to_csv(csvname)
-        return None
-    for i, l in subdf_iter.iterrows():
-        m = DynatreeMeasurement(day=kwds['day'], tree=kwds['tree'], measurement_type=kwds['type'],
-                            measurement=i)
-        logger.info(m)
-        for j in l.iloc[0]:
-            s = SignalTuk(m, start=round(j / 100.0 - 0.04, 2), end=round(j / 100.0 + 0.04, 2), probe=kwds['probe'])
-            tuky[(i, j)] = s.fft
-            del s
-        del m
+        try:
+            subans = ans.xs(key=probe, level="probe", axis=1).sort_index()
+            subans.plot(linewidth=1, legend=None, alpha=0.3, ax=ax)
 
-    ans = pd.DataFrame(tuky)
-    pocet = ans.shape[1]
-    ans[['median', 'std']] = ans.apply(lambda row: [row.median(), row.std()], axis=1).to_list()
-    fig, ax = plt.subplots(figsize=figsize)
-    ans.plot(linewidth=1, legend=None, alpha=0.2, ax=ax)
-    ans.loc[:, ["median"]].plot(ax=ax, color='red')
-    ax.set(yscale='log', title=f"{kwds['type']} {kwds['day']} {kwds['tree']} {kwds['probe']}, data from {pocet} curves",
-           xlabel="Frequency / Hz", ylabel="FFT")
-    # Nastavení mřížky po 100
-    ax.xaxis.set_minor_locator(plt.MultipleLocator(100))
-    ax.grid(which='major', axis='x', color='gray', alpha=0.7, linewidth=2)
-    ax.grid(which='major', axis='y', color='gray', alpha=0.7)
-    ax.grid(which='both', axis='x', color='gray', alpha=0.7)
+            data = subans.apply(lambda row: row.median(), axis=1)
+            data.plot(color='red', ax=ax)
 
-    ax.get_legend().remove()
-    plt.tight_layout()
-    fig.savefig(figname, transparent=True)
-    ans.to_csv(csvname)
-    plt.close('all')
-    return ans
+            ax.set(yscale='log')
+            ax.set(
+                title=f"{kwds['measurement_type']} {kwds['day']} {kwds['tree']} {probe}, data from {subans.shape[1]} curves")
+
+            # Nastavení mřížky po 100
+            ax.xaxis.set_minor_locator(plt.MultipleLocator(100))
+            ax.grid(which='major', axis='x', color='gray', alpha=0.7, linewidth=2)
+            ax.grid(which='major', axis='y', color='gray', alpha=0.7)
+            ax.grid(which='both', axis='x', color='gray', alpha=0.7)
+        except:
+            subans = pd.DataFrame()
+            ax.set(title=f"{kwds['measurement_type']} {kwds['day']} {kwds['tree']} {probe}, no data")
+            logger.info(f"No curve for {probe}")
+
+        ax.set(xlabel="Frequency / Hz", ylabel="FFT")
+        # ax.get_legend().remove()
+        plt.tight_layout()
+        fig.savefig(figname)
+        plt.close('all')
+
+def process_wrapper(row):
+    logger.info(f"Processing {row} by process_wrapper")
+    process_one_tree_to_images(**row)
 
 df = pd.read_csv(config.file['outputs/FFT_acc_knock'])
 df = df[df["valid"] == True]
+df = df.drop(["filename", "valid", "knock_index", "freq"], axis=1)
+df = df.rename({"type": "measurement_type"}, axis=1)
+
+list_of_measurements = df[["measurement_type", "day", "tree"]].drop_duplicates()
+
 def main():
     try:
         matplotlib.use('TkAgg')  # https://stackoverflow.com/questions/39270988/ice-default-io-error-handler-doing-an-exit-pid-errno-32-when-running
     except:
         matplotlib.use('Agg')
 
-    dfm = (get_all_measurements_acc()
-        .drop(["measurement"], axis=1)
-        .drop_duplicates()
-        .reset_index(drop=True)
-        .rename({'date': 'day'}, axis=1) )
-
-    res = progress_map(get_FFT_all_acc_wrapper, [i for _, i in dfm.iterrows()],
-                       n_cpu=5)
-
-def get_FFT_all_acc_wrapper(i):
-    get_FFT_all_acc(**i)
+    # print(list_of_measurements)
+    progress_map(process_wrapper, [i for _, i in list_of_measurements.iterrows()], n_cpu=5)
 
 if __name__ == "__main__":
     # get_FFT_all_acc(**{'day': '2022-08-16', 'tree': 'BK13', 'type': 'normal'})
     # get_FFT_all_acc(**{'day': '2021-06-29', 'tree': 'BK08', 'type': 'normal'})
-    get_FFT_one_probe(**{'day': '2021-06-29', 'tree': 'BK10', 'type': 'normal', 'probe': 'a02_y'})
+    # get_FFT_one_probe(**{'day': '2021-06-29', 'tree': 'BK10', 'type': 'normal', 'probe': 'a02_y'})
     # get_FFT_one_probe(**{'day': '2024-09-02', 'tree': 'BK01', 'type': 'afterro2', 'probe': 'a04_y'})
-    # main()
+    main()
