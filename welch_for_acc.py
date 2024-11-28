41302cf815f3d57da11eabdc58e0598df346d9e4
robert-marik <robert.marik.cz@gmail.com>
2024-11-28 13:31:39 +0100

    paralelizace tvorby welochoa spektra

--- welch_for_acc.py
+++ welch_for_acc.py
@@ -9,9 +9,9 @@ Created on Mon Sep 16 11:24:56 2024
 from dynatree import dynasignal, dynatree, FFT, find_measurements
 import matplotlib.pyplot as plt
 import pandas as pd
-from tqdm import tqdm
 import matplotlib
 import config
+from parallelbar import progress_map
 
 df = find_measurements.get_all_measurements(method='all', type='all')
 df = df[df['measurement'] != 'M01']
@@ -22,8 +22,7 @@ failed_df = pd.read_csv(config.file["FFT_failed"])
 failed = failed_df[failed_df["probe"]==probe].drop(columns=["probe"]).values.tolist()
 
 def do_welch_spectra(row):
-    
-    measurement_type, day, tree = row
+    measurement_type, day, tree = list(row.values())
     measurements = df[
         (df["type"]==row['type']) & 
         (df["day"]==row['day']) &  
@@ -67,22 +66,14 @@ def do_welch_spectra(row):
 
 #%%
 
-
-
 matplotlib.use('Agg')
 tdf = df[["type","day","tree"]].copy().drop_duplicates()
-pbar = tqdm(total=len(tdf))
+tdf_list = tdf.to_dict('records')
 
-for i,row in tdf.iterrows():
+def process_one_row(row):
     fig, ax = do_welch_spectra(row)
     filename = f"../temp/welch/{row['tree']}_{row['day']}_{row['type']}.png"
     fig.savefig(filename)
-    pbar.update(1)
     plt.close('all')
 
-pbar.close()
-    
-    
-#%%
-
-# do_welch_spectra(m)    
+progress_map(process_one_row, tdf_list)
