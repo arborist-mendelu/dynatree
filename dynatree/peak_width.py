74dc657798dd326114620e9fad491e0daafc3a28
Robert Marik <marik@mendelu.cz>
2024-12-17 17:36:56 +0100

    sirka peaku

--- dynatree/peak_width.py
+++ dynatree/peak_width.py
@@ -1,4 +1,68 @@
 from dynatree.dynatree import DynatreeMeasurement
 from dynatree.FFT import DynatreeSignal
 
-def find_peak_width():
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from dynatree import find_measurements
+import pandas as pd
+
+peak_min = .1 # do not look for the peak smaller than this value
+peak_max = 0.7 # do not look for the peak larger than this value
+
+def find_peak_width(m):
+    s = DynatreeSignal(m, "Elasto(90)")
+
+    frequencies = s.fft.index
+    amplitudes = s.fft.values
+    amplitudes[:8] = 0
+    threshold = np.max(amplitudes)/np.sqrt(2)
+
+    amax = np.max(amplitudes)
+    treshold = amax/np.sqrt(2)
+    _ = np.where(amplitudes - treshold > 0)[0]
+    a = _[0]-1
+    b = _[-1]+1
+
+
+    def zero(a):
+        return frequencies[a]- (amplitudes[a]-treshold)*(frequencies[a+1]-frequencies[a])/(amplitudes[a+1]-amplitudes[a])
+
+    ans = (-zero(a) + zero(b-1))/amax
+
+    fig, ax = plt.subplots()
+    ax.plot(frequencies, amplitudes, "o-", label="FFT")
+    plt.hlines(threshold, xmin=zero(a), xmax=zero(b-1), color='red', label="1/sqrt(2)*MAX")
+    ax.set(yscale='log', ylim=(threshold/20, amax*2), xlim = (0.15,0.8),
+           xlabel="freq/Hz", ylabel="Amplitude Elasto",
+          title=f"{m}")
+    ax.grid()
+    ax.legend()
+    return {'width':ans, 'figure':fig}
+
+def process_row(row):
+    date, tree, measurement, measurement_type, optics, day = row
+    m = DynatreeMeasurement(day=day, tree=tree, measurement=measurement, measurement_type=measurement_type)
+    return find_peak_width(m)
+
+
+if __name__ == '__main__':
+    try:
+        matplotlib.use('TkAgg')
+    except:
+        matplotlib.use('Agg')
+    out = {}
+
+    df = find_measurements.get_all_measurements(method='all', type='all')
+    df = df[df["measurement"] != "M01"]
+    for i,row in df.iterrows():
+        try:
+            ans = process_row(row)
+            out[tuple(row[:4])] = ans['width']
+            plt.savefig(f"figs_peak_width/{row['date']}_{row['type']}_{row['tree']}_{row['measurement']}.png")
+            plt.close()
+        except:
+            print(f"Row {row} failed.")
+
+    df = pd.DataFrame(out, index=["width"]).T
+    df.to_csv("../outputs/peak_width.csv")
