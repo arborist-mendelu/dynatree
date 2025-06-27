6cb8ba159987216c4a86024274c9e87dc8a83b31
robert-marik <robert.marik.cz@gmail.com>
2025-06-28 00:18:58 +0200

    Improve image downloader

--- scripts/download_damping_from_api_pt_inclino.py
+++ scripts/download_damping_from_api_pt_inclino.py
@@ -1,5 +1,8 @@
 import os
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 import pandas as pd
 import requests
 from urllib.parse import urlencode
@@ -7,10 +10,9 @@ from PIL import Image
 from io import BytesIO
 from dynatree import find_measurements
 
-# Probe
-probes = ["Pt3", "Pt4", "blueMaj", "yellowMaj"]
-probe = probes[3]
 
+# Probe
+probes = ["Elasto(90)","Pt3", "Pt4", "blueMaj", "yellowMaj"]
 
 # Načtení tabulky df
 df = find_measurements.get_all_measurements(method='all', type='all')
@@ -31,44 +33,45 @@ manual_data = [tuple(i) for i in manual_data]
 for _, row in df.iterrows():
     if row["measurement"] == "M01":
         continue
-    
-    params = {
-        "method": f"{row['day']}_{row['type']}",
-        "tree": row["tree"],
-        "measurement": row["measurement"],
-        "probe": probe,
-        "format": "png",
-        "damping_method": "extrema"
-    }
 
-#    if not ( (row['type'], row['day'], row['tree'], row['measurement']) in manual_data ):
-#    if not row['day'] == "2025-04-01":
-#        continue
+    for probe in probes:
+        params = {
+            "method": f"{row['day']}_{row['type']}",
+            "tree": row["tree"],
+            "measurement": row["measurement"],
+            "probe": probe,
+            "format": "png",
+            "damping_method": "defmulti"
+        }
 
-    url1 = f"{base_url1}?{urlencode(params)}"
-    url2 = f"{base_url2}?{urlencode(params)}"
-    
-    filename = f"{params['method']}_{row['tree']}_{row['measurement']}_{probe}.png"
-    filepath = os.path.join(output_dir, filename)
-    
-    try:
-        response1 = requests.get(url1, timeout=10)
-        response1.raise_for_status()
-        img1 = Image.open(BytesIO(response1.content))
+    #    if not ( (row['type'], row['day'], row['tree'], row['measurement']) in manual_data ):
+    #    if not row['day'] == "2025-04-01":
+    #        continue
+
+        url1 = f"{base_url1}?{urlencode(params)}"
+        url2 = f"{base_url2}?{urlencode(params)}"
         
-        response2 = requests.get(url2, timeout=10)
-        response2.raise_for_status()
-        img2 = Image.open(BytesIO(response2.content))
+        filename = f"{params['method']}_{row['tree']}_{row['measurement']}_{probe}.png"
+        filepath = os.path.join(output_dir, filename)
         
-        # Spojení obrázků pod sebe
-        width = max(img1.width, img2.width)
-        height = img1.height + img2.height
-        combined_img = Image.new("RGB", (width, height))
-        combined_img.paste(img1, (0, 0))
-        combined_img.paste(img2, (0, img1.height))
-        
-        # Uložení výsledného obrázku
-        combined_img.save(filepath)
-        print(f"Uloženo: {filename}")
-    except requests.RequestException as e:
-        print(f"Chyba při stahování obrázků: {e}")
+        try:
+            response1 = requests.get(url1, timeout=10)
+            response1.raise_for_status()
+            img1 = Image.open(BytesIO(response1.content))
+            
+            response2 = requests.get(url2, timeout=10)
+            response2.raise_for_status()
+            img2 = Image.open(BytesIO(response2.content))
+            
+            # Spojení obrázků pod sebe
+            width = max(img1.width, img2.width)
+            height = img1.height + img2.height
+            combined_img = Image.new("RGB", (width, height))
+            combined_img.paste(img1, (0, 0))
+            combined_img.paste(img2, (0, img1.height))
+            
+            # Uložení výsledného obrázku
+            combined_img.save(filepath)
+            logging.info(f"Uloženo: {filename}")
+        except requests.RequestException as e:
+            logging.error(f"Chyba při stahování obrázků: {e}")
